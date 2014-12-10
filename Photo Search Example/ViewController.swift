ce0d9aee7f9e057ceb32d590cdac921c526fc593
louiskonig <louiskonig@gmail.com>
2014-12-09 23:12:03 -0500

    committed

--- Photo Search Example/ViewController.swift
+++ Photo Search Example/ViewController.swift
@@ -8,11 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UISearchBarDelegate {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( "https://api.instagram.com/v1/tags/patriots/media/recent?client_id=93faaf1832b247609747b2719f423cac",
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+            
+                if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
+                    self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
+                    for var i = 0; i < dataArray.count; i++ {
+                        let dataObject: AnyObject = dataArray[i]
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            println("image " + String(i) + " URL is " + imageURLString)
+                            
+                            let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString)!)
+                            let imageView = UIImageView(image: UIImage(data: imageData!))
+                            let centerX = CGRectGetMidX(self.scrollView.frame)
+                            imageView.frame = CGRectMake(centerX-160, CGFloat(320*i), 320, 320)
+                            self.scrollView.addSubview(imageView)
+                        }
+                    }
+                }
+            
+            
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +54,44 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func searchBarSearchButtonClicked(searchBar: UISearchBar!) {
+        for subview in self.scrollView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        searchBar.resignFirstResponder()
+        
+        let instagramURLString = "https://api.instagram.com/v1/tags/" + searchBar.text + "/media/recent?client_id=93faaf1832b247609747b2719f423cac"
+        
+        let manager = AFHTTPRequestOperationManager()
+        
+        manager.GET( instagramURLString,
+            parameters: nil,
+            success: { (operation: AFHTTPRequestOperation!,responseObject: AnyObject!) in
+                println("JSON: " + responseObject.description)
+                
+                if let dataArray = responseObject.valueForKey("data") as? [AnyObject] {
+                    self.scrollView.contentSize = CGSizeMake(320, CGFloat(320*dataArray.count))
+                    for var i = 0; i < dataArray.count; i++ {
+                        let dataObject: AnyObject = dataArray[i]
+                        if let imageURLString = dataObject.valueForKeyPath("images.standard_resolution.url") as? String {
+                            println("image " + String(i) + " URL is " + imageURLString)
+                            
+                            let imageData =  NSData(contentsOfURL: NSURL(string: imageURLString)!)
+                            let imageView = UIImageView(image: UIImage(data: imageData!))
+                            imageView.frame = CGRectMake(0, CGFloat(320*i), 320, 320)
+                            self.scrollView.addSubview(imageView)
+                        }
+                    }
+                }
+            },
+            failure: { (operation: AFHTTPRequestOperation!,error: NSError!) in
+                println("Error: " + error.localizedDescription)
+        })
+    }
 
+    
+    
+    
 }
 
---
+++ bridge.h
@@ -0,0 +1,14 @@
+//
+//  bridge.h
+//  Photo Search Example
+//
+//  Created by Louis Konig on 12/7/14.
+//  Copyright (c) 2014 Louis Konig. All rights reserved.
+//
+
+#ifndef Photo_Search_Example_bridge_h
+#define Photo_Search_Example_bridge_h
+#import "AFNetworking/AFNetworking.h"
+
+
+#endif
